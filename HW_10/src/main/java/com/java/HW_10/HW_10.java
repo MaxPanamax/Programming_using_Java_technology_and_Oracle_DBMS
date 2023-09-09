2d422585aa2aebb46114f4a580bf3ba7ae393358
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-09-09 16:37:26 +0200

    Programming_using_Java_technology_and_Oracle_DBMS
    Programming_using_Java_technology_and_Oracle_DBMS

---
+++ HW_10/src/main/java/com/java/HW_10/HW_10.java
@@ -0,0 +1,15 @@
+package com.java.HW_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class HW_10 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HW_10.class, args);
+	}
+
+}
---
+++ HW_10/src/test/java/com/java/HW_10/HomeWork15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.HW_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
